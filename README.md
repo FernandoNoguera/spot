39894e741d1441215cc0ccfb4ab510739f7baeb4
FernandoNoguera <fnoguerav25@gmail.com>
2023-05-08 18:18:40 -0400

    feat: add initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# Mi Aplicación FastAPI SPOT
+
+## Descripción
+
+Esta es una aplicación es una test de ingreso para SPOT.
+
+## Deploment y documentación del API
+
+[Deployment](https://api.spot.maelstrom-digital.cl/)
+[Postman documentation](https://documenter.getpostman.com/view/14482878/2s93eYWY8g)
+[documentación Swagger UI ](https://api.spot.maelstrom-digital.cl/docs)
+[Documentacion ReDoc](https://api.spot.maelstrom-digital.cl/redoc)
+
+##Requisitos
+- Python 3.7 o superior
+- pip
+- [Opcional] Docker 
+
+## Instalación
+
+Clona el repositorio:
+``` 
+git clone https://github.com/FernandoNoguera/spot.git
+``` 
+
+Navega hasta la carpeta del proyecto:
+``` 
+cd spot
+``` 
+
+
+Crea un entorno virtual y actívalo:
+``` 
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+Instala las dependencias:
+```
+pip install -r requirements.txt
+``` 
+
+## Uso
+
+Activa el entorno virtual:
+``` 
+source .venv/bin/activate
+``` 
+
+Inicia el servidor de desarrollo de FastAPI:
+
+``` 
+uvicorn app.main:app --reload
+``` 
+
+Abre un navegador web y visita [/docs](http://localhost:8000/docs) para acceder a la documentación de la API.
+
+Para detener el servidor, presiona Ctrl + C.
+
+## Docker
+
+También puedes ejecutar la aplicación utilizando Docker
+
+Construye la imagen de Docker:
+``` 
+docker build -t my-fastapi-app .
+``` 
+
+Inicia el contenedor:
+``` 
+docker run -d --name my-fastapi-container -p 8003:80 my-fastapi-app
+``` 
+Abre un navegador web y visita [/docs](http://localhost:8000/docs) para acceder a la documentación de la API.
+Para detener el contenedor, presiona Ctrl + C y luego ejecuta:
+
+``` 
+docker down
+``` 
+
+## Test
+
+También puedes ejecutar test:
+
+``` 
+pytest test_app.py 
+``` 
+
+
+## Contribución
+¡Las contribuciones son bienvenidas! Si deseas colaborar en este proyecto, por favor, abre un "pull request" en GitHub.
+
+
---
+++ main.py
@@ -0,0 +1,115 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from azure.storage.blob import BlobServiceClient
+from fastapi.middleware.cors import CORSMiddleware
+import asyncpg
+import uuid
+import base64
+import os
+import asyncpg
+from os.path import join, dirname
+from dotenv import load_dotenv
+from datetime import datetime
+
+# Dotenv
+dotenv_path = join(dirname(__file__), '.env.dev')
+load_dotenv(dotenv_path)
+load_dotenv(dotenv_path, verbose=True)
+
+
+# Modelos
+class Evento(BaseModel):
+    fecha: datetime
+    imagen: str
+    camara_id: str
+
+class BatchEventos(BaseModel):
+    eventos: List[Evento]
+
+
+# Configuración de la base de datos
+USER_DB = os.environ.get("USER_DB")
+PASSWORD_DB = os.environ.get("PASSWORD_DB")
+HOST_DB = os.environ.get("HOST_DB")
+PORT_DB = os.environ.get("PORT_DB")
+NAME_DB = os.environ.get("NAME_DB")
+
+async def create_tables():
+    conn = await asyncpg.connect(
+        user=USER_DB,
+        password=PASSWORD_DB,
+        database=NAME_DB,
+        host=HOST_DB
+    )
+    await conn.execute('''
+        CREATE TABLE IF NOT EXISTS events (
+            id bigserial PRIMARY KEY,
+            date_utc timestamp with time zone,
+            camera_id text,
+            image_url text
+        );
+    ''')
+    await conn.close()
+dsn = f'postgresql://{USER_DB}:{PASSWORD_DB}@{HOST_DB}:{PORT_DB}/{NAME_DB}'
+async def connect():
+    conn = await asyncpg.connect(dsn)
+    return conn
+
+
+# Configuración de la aplicación
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await create_tables()
+
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Conexión a Azure Blob Storage
+CONTAINER_ABS = os.environ.get("CONTAINER_ABS")
+PASSWORD_ABS = os.environ.get("PASSWORD_ABS")
+CONN_STR = os.environ.get("CONN_STR")
+conn_str = CONN_STR
+container_name = CONTAINER_ABS
+blob_service_client = BlobServiceClient.from_connection_string(conn_str)
+container_client = blob_service_client.get_container_client(container_name)
+
+# Endpoint para subir imágenes en batch
+@app.post("/subir_imagenes")
+async def subir_imagenes(eventos: BatchEventos):
+    conn = await connect()
+    values = []
+    for evento in eventos.eventos:
+        # Decodificar imagen en base64
+        img_bytes = base64.b64decode(evento.imagen)
+        # Generar nombre de archivo único
+        nombre_archivo = f"{uuid.uuid4()}.jpg"
+        # Crear archivo en disco
+        with open(nombre_archivo, "wb") as f:
+            f.write(img_bytes)
+        # Subir archivo a Azure Blob Storage
+        blob_client = container_client.get_blob_client(nombre_archivo)
+        with open(nombre_archivo, "rb") as data:
+            blob_client.upload_blob(data)
+        # Eliminar archivo temporal
+        os.remove(nombre_archivo)
+        # Guardar datos en PostgreSQL
+        url = f"https://{blob_service_client.account_name}.blob.core.windows.net/{container_name}/{nombre_archivo}"
+        values.append((evento.fecha, evento.camara_id, url))
+    query = "INSERT INTO events (date_utc, camera_id, image_url) VALUES ($1, $2, $3)"
+    await conn.executemany(query, values)
+    await conn.close()
+    return {"mensaje": "Imágenes subidas correctamente"}
